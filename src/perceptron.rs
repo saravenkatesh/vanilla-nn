21ab67a06b1c1c39c3e262788a7d2f52d4314dca
Sara Venkatesh <sarajvenkatesh@gmail.com>
2023-10-04 20:21:21 -0700

    move all functions into same impl

--- src/perceptron.rs
+++ src/perceptron.rs
@@ -13,27 +13,21 @@ pub struct Perceptron<const X: usize, const N: usize> {
 }
 
 impl<const X: usize, const N: usize> Perceptron<X, N> {
-    pub fn classifier(&self, input: SVector<f64, X>) -> f64 {
-        threshold(self.weights.dot(&input) + self.bias)
-    }
-}
-
-impl<const X: usize, const N: usize> Perceptron<X, N> {
     fn one_step(
         &mut self, 
         inputs: SMatrix<f64, X, N>, 
         outputs: SVector<f64, N>,
         step: f64, 
-    )   {
-            // Update weights
-            let mut new_weights: Vec<f64> = Vec::new();
-            for i in 0..X {
-                let mut new_weight: f64 = self.weights[i];
-                for (j, d) in inputs.column_iter().enumerate() {
-                    new_weight -= step * (threshold(self.weights.dot(&d) + self.bias) - outputs[j]) * d[i];
-                }
-                new_weights.push(new_weight);
+    ) {
+        // Update weights
+        let mut new_weights: Vec<f64> = Vec::new();
+        for i in 0..X {
+            let mut new_weight: f64 = self.weights[i];
+            for (j, d) in inputs.column_iter().enumerate() {
+                new_weight -= step * (threshold(self.weights.dot(&d) + self.bias) - outputs[j]) * d[i];
             }
+            new_weights.push(new_weight);
+        }
         self.weights = SVector::<f64, X>::from_vec(new_weights);
         
         // Update bias
@@ -41,9 +35,7 @@ impl<const X: usize, const N: usize> Perceptron<X, N> {
             self.bias -=step * (threshold(self.weights.dot(&d) + self.bias) - outputs[j])   
         }
     }
-}
 
-impl<const X: usize, const N: usize> Perceptron<X, N> {
     pub fn gradient_descent(
         &mut self, 
         num_steps: i32,
@@ -58,5 +50,9 @@ impl<const X: usize, const N: usize> Perceptron<X, N> {
                 step, 
             );
         }
+    }
+    
+    pub fn classifier(&self, input: SVector<f64, X>) -> f64 {
+        threshold(self.weights.dot(&input) + self.bias)
     }
 }
