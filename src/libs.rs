b3f3ca4a0e18a21d7a415cda5bf9090ece4c8f42
Sara Venkatesh <sarajvenkatesh@gmail.com>
2023-10-09 08:18:27 -0700

    initalize net working, yay

--- src/libs.rs
+++ src/libs.rs
@@ -1,5 +1,30 @@
-use nalgebra::{DVector, DMatrix};
+use nalgebra::{DVector, DMatrix, dmatrix, dvector};
 use libm::exp;
+
+fn main() {
+    // A (2, 2, 1) - network
+    let feed_forward = FeedForward{
+        weights: vec!(
+            dmatrix![
+                3.0, 0.5;
+                1.0, 0.2;
+            ],
+            dmatrix![0.1, 0.0;
+                0.2, 0.2],
+            dmatrix![0.3, 0.1],
+        ),
+        bias: vec!(
+            dvector!(1.0, 0.3),
+            dvector!(0.3, 0.5),
+            dvector!(0.2),
+        ),
+        size: 3,
+    };
+    
+    let input: DVector<f64> = dvector![1.0, -1.0];
+    let output: DVector<f64> = dvector![2.0];
+    println!("{:?}", feed_forward.update(input, output));
+}
 
 // TODO: replace for loops with maps
 fn sigmoid(mut x: f64) -> f64 {
@@ -40,8 +65,16 @@ impl FeedForward {
     fn update(
         self,
         input: DVector<f64>,
-        outputs: Vec<DVector<f64>>,
+        training_output: DVector<f64>,
     ) -> (Vec<DMatrix<f64>>, Vec<DVector<f64>>) {
+        /// Takes a single input vector and single desired output
+        /// vector as training data.  Returns the gradient of
+        /// the cost function w.r.t the network's weights
+        /// and biases, for this one input / output pair
+        /// i.e. C(input, training_output) = 
+        /// ( N(input) - training_output) / 2, 
+        /// where N(x) is the function described by the network. 
+        
         // Vector to store the derivatives w.r.t. bias
         let mut change_in_bias: Vec<DVector<f64>> = Vec::new();
         // Vector to store the derivatives w.r.t weights
@@ -102,16 +135,13 @@ impl FeedForward {
     ) -> (Vec<DVector<f64>>, Vec<DVector<f64>>) {
         for i in 0..self.size {
             // TODO: Understand how refs work
-            let ref w = &self.weights[self.size - 1];
-            let ref b = &self.bias[self.size - i];
+            let ref w = &self.weights[i];
+            let ref b = &self.bias[i];
             let ref output = &layer_outputs[i];
-            
             let linear_component = (*w) * (*output) + (*b);
             linear_components.push(linear_component.clone());
             layer_outputs.push(multi_dim_sigmoid(linear_component));
         }
-        linear_components.reverse();
-        layer_outputs.reverse();
         return (layer_outputs, linear_components)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,20 @@ const NUM_STEPS: i32 = 10;
 
 
 fn main() {
-    let mut perceptron = Perceptron{
-        weights: SVector::<f64, 2>::new(0.0, 1.0),
-        bias: 3.,
+    let feed_forward = FeedForward{
+        weights: vec!(
+            dmatrix![
+                0.0, 0.0;
+                0.0, 0.0;
+            ],
+            dmatrix![0.0, 0.0],
+        ),
+        bias: vec!(
+            dvector!(0.0, 0.0),
+            dvector!(0.0),
+        ),
+        size: 2,
     };
-    
-    perceptron.gradient_descent(
-        NUM_STEPS,
-        INPUTS,
-        OUTPUTS,
-        STEPS,
-    );
-
-    loop {
-        println!("Enter a 2 dimensional vector");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input");
-        let sequence: Vec<f64> = 
-            input.split(",")
-                .map(|s| s.trim())
-                .filter(|s| !s.is_empty())
-                .map(|s| s.parse().unwrap())
-                .collect();
-        let data: SVector<f64, 2> = SVector::<f64, 2>::from_vec(sequence);
-        
-        let classification: f64 = perceptron.classifier(data);
-        println!("{}", classification);
-    }
 }
 
 
